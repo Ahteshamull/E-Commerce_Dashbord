91897cbbfadb06f41db4c06d315ee4e2fb7aa163
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-05-04 13:00:40 +0600

    logo add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-
+import  {Link}  from 'react-router'
+import Logo from '../assets/images/Dashboard.png'
 
 export default function Header() {
   return (
-    <header>
-        <div className='container-fluid'>
-            <h1>Header</h1>
-            <button className="btn btn-danger ">hello</button>
+    <header className='d-flex align-items-center'>
+        <div className='container-fluid w-100'>
+            <div className='row d-flex align-items-center'>
+              <div className='col-xs-3'>
+               <Link to='/'>
+               <img src={Logo} className='logo' alt="Logo" />
+               </Link>
+              </div>
+            </div>
         </div>
       
     </header>
