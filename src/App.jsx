3c96e5bbaaeb9eef735cbd8c5575ae3044685047
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-05-04 12:06:51 +0600

    all setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
+import {Routes,Route} from 'react-router-dom'
+
 export default function App() {
   return (
-    <div className="text-3xl font-bold font-tag underline">
+    <div style={{fontFamily:"Tagesschrift"}} className="text-3xl">
       hasan
     </div>
   )
