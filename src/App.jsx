2581a280705118838428e02145ffb34540d6fcc5
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-05-28 20:08:33 +0600

    Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
 import React from 'react'
-import './App.css'
-import Dashboard from './pages/Dashboard'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import {Routes,Route,BrowserRouter} from 'react-router-dom'
-import Header from './components/Header'
-import "bootstrap/dist/css/bootstrap.min.css";
-import Notification from './pages/Notification'
 
 export default function App() {
   return (
-    <BrowserRouter>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Dashboard/>}/>
-      <Route path='/dashboard' element={<Dashboard/>}/>
-      <Route path='/notification' element={<Notification/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-    </Routes>
-   </BrowserRouter>
+    <div>
+      dashboard
+    </div>
   )
 }
