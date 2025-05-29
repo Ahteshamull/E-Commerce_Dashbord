9ab3f33e9f239fa01a017d6970f447ac2513b269
Hasan <158465711+Ahteshamull@users.noreply.github.com>
2025-05-29 12:44:43 +0600

    Font Familly Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './App.css'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import {Routes,Route,BrowserRouter} from 'react-router-dom'
+import Header from './components/Header'
+import "bootstrap/dist/css/bootstrap.min.css";
+import Notification from './pages/Notification'
 
 export default function App() {
   return (
-    <div>
-      dashboard
-    </div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Dashboard/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/notification' element={<Notification/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+    </Routes>
+   </BrowserRouter>
   )
 }
